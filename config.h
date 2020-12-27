ccc457eeed08c56497df9a084e2edfb0b537ea4a
gh0o5t <gh0o5t@protonmail.com>
2020-12-27 13:32:07 +0100

    Changing some colors

--- config.h
+++ config.h
@@ -12,11 +12,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Sauce Code Pro NF:size=10" };
 static const char dmenufont[]       = "Sauce Code Pro NF:size=10";
-static const char col_gray1[]       = "#222222";
+//static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#81a1c1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
