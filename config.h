4a911c1905786c730c258485b8048b16c0b79859
gh0o5t <gh0o5t@protonmail.com>
2020-12-30 14:15:46 +0100

    Changing terminal incon on bar.

--- config.h
+++ config.h
@@ -29,8 +29,8 @@ static const char *colors[][3]      = {
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 //static const char *alttags[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9" };
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
-static const char *alttags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *alttags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
