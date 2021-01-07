272012a50bc971caff0c4ec52074930386aac85a
gh0o5t <gh0o5t@protonmail.com>
2021-01-07 11:57:29 +0100

    Changed font type

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Sauce Code Pro NF:size=10" };
-static const char dmenufont[]       = "Sauce Code Pro NF:size=10";
+static const char *fonts[]          = { "SauceCodePro Nerd Font:size=10" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font:size=10";
 //static const char col_gray1[]       = "#222222";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#444444";
