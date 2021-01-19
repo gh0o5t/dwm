d054ae322aad8c6e6a48847a603420ed0c97bc6a
gh0o5t <gh0o5t@protonmail.com>
2021-01-19 19:36:01 +0100

    Switched to Brave

--- config.h
+++ config.h
@@ -73,14 +73,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *firefox[]  = { "firefox", NULL };
+static const char *brave[]  = { "brave-browser", NULL };
 static const char *slock[]  = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = brave } },
 	{ MODKEY,                       XK_Escape, spawn,          {.v = slock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
