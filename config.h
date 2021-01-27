79ec2572b032d2e50e27f7fa1b029e4e6da7c559
gh0o5t <gh0o5t@protonmail.com>
2021-01-27 16:49:55 +0100

    Changed font, alttags, terminal

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fantasque Sans Mono:size=12" };
-static const char dmenufont[]       = "Fantasque Sans Mono:size=12";
+static const char *fonts[]          = { "Hack Nerd Font:size=13" };
+static const char dmenufont[]       = "Hack Nerd Font:size=12";
 //static const char col_gray1[]       = "#222222";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#444444";
@@ -28,7 +28,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *alttags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *alttags[] = { "◆1", "◆2", "◆3", "◆4", "◆5", "◆6", "◆7", "◆8", "◆9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -71,8 +71,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *brave[]  = { "brave-browser", NULL };
 static const char *slock[]  = { "slock", NULL };
 
